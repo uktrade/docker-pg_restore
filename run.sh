bc3e67603a4bd29d116e633093dab6e556104400
Bowen Sun <bowensun@gmail.com>
2018-04-18 15:37:23 +0100

    fix variables

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ date
 DATE=$(date +%Y-%m-%d)
 aws s3 cp --only-show-errors $S3_BUCKET/$DATE.psql.tar /tmp/$DATE.psql.tar
 echo "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$PGDATABASE' AND pid <> pg_backend_pid();" | psql -h $PGHOST -U $PGUSER
-pg_restore -h $DEST_PGHOST -U $DEST_PGUSER -d temp -x -O -c -C --if-exists -v $DUMP_PATH/$DATE.psql.tar
+pg_restore -h $PGHOST -U $PGUSER -d $PGDATABASE -x -O -c -C --if-exists -v /tmp/$DATE.psql.tar
 rm -f /tmp/$DATE.psql.tar
