2fe6a0bd837fbb50d6f138a0d74beb3390dde246
Bowen Sun <nusnewob@users.noreply.github.com>
2018-04-25 15:29:48 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ export $(cat /.env | xargs)
 
 date
 
-DATE=$(date +%Y-%m-%d)
+DATE=$(date +%Y-%m-%d -d "yesterday")
 aws s3 cp --only-show-errors $S3_BUCKET/$DATE.psql.tar /tmp/$DATE.psql.tar
 echo "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$PGDATABASE' AND pid <> pg_backend_pid();" | psql -h $PGHOST -U $PGUSER
 pg_restore -h $PGHOST -U $PGUSER -d $PGDATABASE -x -O -c -C --if-exists -v /tmp/$DATE.psql.tar
