0374f4b23a2987db42c456ec42779a19562a65c8
Bowen Sun <nusnewob@users.noreply.github.com>
2018-04-26 09:44:05 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -x
 
 env -
-export $(cat /.env | xargs)
+source /.env
+
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+export PGPASSWORD=$PGPASSWORD
 
 date
 
